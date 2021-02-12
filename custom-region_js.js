c3a3c9ccabfcfa4e01d37e68cd858390c65ff09f
Kshitiz <kshitiz.stha11@gmail.com>
2021-02-12 13:06:29 +0545

    initial commit

---
+++ custom-region_js.js
@@ -0,0 +1,15 @@
+//@ts-check
+// Using Typescipt without ts & use f8 to shift from one error to another 
+
+console.blog("hello");
+console.log("hello");
+console.blog("hello");
+// =================================================== 
+// USING REGION IN JS
+//#region Ram bahadur
+console.log("hello");
+//#endregion
+
+
+
+// ===============================================
